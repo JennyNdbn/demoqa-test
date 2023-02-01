c6508db3b76b1d1e7d4e3d9b95dd1f919c9ce26d
JennyNdbn <NadobnayaEA@yandex.ru>
2023-02-01 13:59:28 +0700

    added Properties

--- src/test/java/guru/qa/tests/TestBaseExtended.java
+++ src/test/java/guru/qa/tests/TestBaseExtended.java
@@ -18,10 +18,10 @@ public class TestBaseExtended {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.remote = System.getProperty("remote");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
